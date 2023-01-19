a0131dc8808959d74df49fc7aff599fbb52db2a0
Lotte van der Klei <115008562+lottevanderklei2@users.noreply.github.com>
2023-01-19 09:41:39 +0100

    Update streamlit_app.py
    terugzetten zonder emoji

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,8 @@
 import streamlit
-import emoji
 
 streamlit.title('My Parents New Healty Diner')
 
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text(emoji.emojize(":chicken:")+'Kale, Spinach & Rocket Smoothie')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
